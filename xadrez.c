3e6943d32381958ee4934c72648e0a2e9e262f7e
Maikel <maikelfullstack@gmail.com>
2025-11-03 21:25:54 +0000

    Novice challenge implemented

--- xadrez.c
+++ xadrez.c
@@ -1,38 +1,36 @@
+
 #include <stdio.h>
 #include  <stdlib.h>
 
-int main (){
-	char peca[20];
-	char direcao[20];
-	int casas;
-	int i = 0;
-	
-	printf("Digite qual pe�a voce quer mover\n");
-	scanf("%s",peca);
-	printf("Para qual lado deseja mover?\n");
-	scanf("%s",direcao);
-	printf("Quantas casas?\n");
-	scanf("%d", &casas);
-	
-	if (strcmp(peca, "bispo") == 0) {
-		printf("Movendo %s, na diagonal %s, %d casas\n", peca, direcao, casas);
-	}
-	else if (strcmp(peca, "torre") == 0) {
-		printf("Movendo %s, na horizontal %s, %d casas\n", peca, direcao, casas);
-	}
-	else if (strcmp(peca, "cavalo") == 0) {
-		printf("Movendo %s, na vertical %s, %d casas\n", peca, direcao, casas);
-	}
-	else if (strcmp(peca, "peao") == 0) {
-		printf("Movendo %s, na diagonal %s, %d casas\n", peca, direcao, casas);
-		casas--;
-	}
-	else if (strcmp(peca, "rainha") == 0) {
-		printf("Movendo %s, em qualquer direção %s, %d casas\n", peca, direcao, casas);
-	}
-	else if (strcmp(peca, "rei") == 0) {
-		printf("Movendo %s, uma casa para %s\n", peca, direcao);
-	}
 
-	
-	return 0;
+int main (){
+int escolha, i=0;
+printf("Escolha a peca para mover:\n1 - Torre\n2 - Bispo\n3 - Rainha\n");
+scanf("%d", &escolha);
+
+switch (escolha) {
+    case 1: // Torre
+        for (i = 0; i < 5; i++) {
+            printf("Torre: Direita\n");
+        }
+        break;
+    case 2: // Bispo
+        i = 0;
+        while (i < 5) {
+            printf("Bispo: Cima, Direita\n");
+            i++;
+        }
+        break;
+    case 3: // Rainha
+        i = 0;
+        do {
+            printf("Rainha: Esquerda\n");
+            i++;
+        } while (i < 8);
+        break;
+    default:
+        printf("Opcao invalida.\n");
+}
+
+return 0;
+}
